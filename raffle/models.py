fc5d1941984f6cd7b8e33c0d33e251fb30707209
Paula Grangeiro <pgrangeiro.dev@gmail.com>
2015-07-12 23:18:00 -0300

    Raffle model and test

---
+++ raffle/models.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import random
+
+
+class Raffle(object):
+
+    def __init__(self, items):
+        self.items = items
+        random.shuffle(self.items)
+
+    def __iter__(self):
+        return (item for item in self.items)
+
+    def __next__(self):
+        try:
+            return self.items.pop(0)
+        except IndexError:
+            raise StopIteration
---
+++ raffle/tests/test_models.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from unittest import TestCase
+
+from raffle.models import Raffle
+
+
+class TestRaffle(TestCase):
+
+    def setUp(self):
+        self.model = Raffle
+        self.instance = self.model([u'a', u'b', u'c'])
+
+    def test_initializes_raffle_correctly(self):
+        instance = self.model([u'a', u'b', u'c'])
+        self.assertIn(u'a', instance.items)
+        self.assertIn(u'b', instance.items)
+        self.assertIn(u'c', instance.items)
+        self.assertEqual(3, len(instance.items))
+
+    def test_getitem_push_item_from_list(self):
+        raffled_item = next(self.instance)
+        self.assertNotIn(raffled_item, self.instance.items)
+
+    def test_getitem_raises_error_if_raffle_has_no_more_items(self):
+        instance = self.model([])
+        self.assertRaises(StopIteration, next, instance)
